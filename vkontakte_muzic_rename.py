ff098fa3cd23dcb3a83bf86e2456f787a10ff6e7
Anton <anton9121@gmail.com>
2013-03-27 15:42:34 +0600

    up

--- vkontakte_muzic_rename.py
+++ vkontakte_muzic_rename.py
@@ -10,6 +10,29 @@ if len(input_files) == 0:
 	print "Files not found!"
 	sys.exit(0)
 
+def get_info(s):
+	print 'FFprobe: open',s
+	app = 'ffprobe'
+	app_path = os.path.join(_path,'bin',app)
+	atr = [ app_path,
+				'-i','"'+s.replace("`","\`")+'"',
+				'-print_format','json'
+	]
+	process = subprocess.Popen((' ').join(atr), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	arr = {'Name':'','Artist':''}
+	while True:
+		buff = process.stdout.readline().replace('\r','').replace('\n','')
+		if buff == '' and process.poll() != None: 
+			break
+		t = buff.split(' : ',1)
+		if len(t) == 2:
+			if t[0].strip() == "title":
+				arr['Name'] = t[1].strip()
+			if t[0].strip() == "artist":
+				arr['Artist'] = t[1].strip()
+	process.wait()
+	return arr
+
 def ffmpeg(s,d,track):
 	d_tmp = d+'.converting.mp3'
 	app = 'ffmpeg'
@@ -38,5 +61,7 @@ for file in input_files:
 	f_name = os.path.splitext(os.path.basename(file))[0]
 	params = f_name.split(' – ',1)
 	if len(params) > 1:
-		params = {'Name':params[0],'Artist':params[1]}
+		params = {'Name':params[1],'Artist':params[0]}
+	#params = get_info(file)
+	#params = {'Name':params['Name'],'Artist':params['Artist']}
 	ffmpeg(file,os.path.join(_path,'mp3',f_name),params)
